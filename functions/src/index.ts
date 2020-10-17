11ded03e4c85460021c35a1fa72c5a4c8cb695ec
nam-t24 <namtruong831@gmail.com>
2020-10-17 18:19:28 -0500

    Second endpoint created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,11 +18,12 @@ app.get("/hello", (request: Request, response: Response) => {
 // import functions
 import { hello_first, first_last } from "./api/template";
 
-import { hello_Nam } from "./api/NamT";
+import { hello_Nam, nam_truong } from "./api/NamT";
 
 app.get("/first-last", first_last);
 app.get("/hello-first-last/:name", hello_first);
 app.get("/hello-temoc-utd/:name", hello_Nam);
+app.get("/temoc-utd", nam_truong);
 
 /* Change things above this line */
 
