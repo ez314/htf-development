22ec85de4988baa38612ca29518d6b5d0e7156d6
kree666 <19652693+kree666@users.noreply.github.com>
2020-10-20 00:54:55 -0500

    Update index.ts
    extra endpoint added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,13 +17,15 @@ app.get("/hello", (request: Request, response: Response) => {
 
 // import functions
 import { hello_first, first_last } from "./api/template";
-import { hello_EmilyB } from "./api/EmilyB";
+import { hello_EmilyB, emily_basile } from "./api/EmilyB";
 
 app.get("/first-last", first_last);
 
 //add your endpoint in addition to the default one
 app.get("/hello-first-last/:name", hello_first);
 app.get("/hello-EmilyB/ :name", hello_EmilyB);
+app.get("/emily-basile", emily_basile);
+
 
 /* Change things above this line */
 
